c98595c661fb638f002768d42f33a550dc22c978
Azleur <ribonucleasa@gmail.com>
2019-11-17 16:42:56 +0100

    Add covariance calculation types and functions; add Bessel's correction; make everything a type; make Validation tolerance optional and provide default.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,21 @@
-import { Stats, Validation, Observe, GetIngestor, Validate, NullStats } from './index';
+import {
+    Stats,
+    Validation,
+    Observe,
+    GetIngestor,
+    Validate,
+    NullStats,
+    ObserveCovariance,
+    GetCovarianceIngestor,
+    NullCovarianceStats,
+    CovarianceValidate
+} from './index';
 
-test("Hi, I'm an example", () => {
+test("Basic Math.random() example", () => {
     const stats = Observe(Math.random, 10000);
-    console.log(stats);
+    // console.log(stats);
     const validation: Validation = {
-        tolerance: 0.01,
+        tolerance: 0.1,
         min: 0,
         max: 1,
         mean: 0.5,
@@ -30,27 +41,28 @@ test("Observe() Stats of a source of numbers by calling it a fixed number of tim
     );
 
     const zeroStats = helper(zeros);
-    expect(zeroStats.min).toBe(0);
-    expect(zeroStats.max).toBe(0);
-    expect(zeroStats.mean).toBe(0);
-    expect(zeroStats.variance).toBe(0);
+    expect(zeroStats.min).toBeCloseTo(0);
+    expect(zeroStats.max).toBeCloseTo(0);
+    expect(zeroStats.mean).toBeCloseTo(0);
+    expect(zeroStats.variance).toBeCloseTo(0);
 
     const oneStats = helper(ones);
-    expect(oneStats.min).toBe(1);
-    expect(oneStats.max).toBe(1);
-    expect(oneStats.mean).toBe(1);
-    expect(oneStats.variance).toBe(0);
+    expect(oneStats.min).toBeCloseTo(1);
+    expect(oneStats.max).toBeCloseTo(1);
+    expect(oneStats.mean).toBeCloseTo(1);
+    expect(oneStats.variance).toBeCloseTo(0);
 
     const halfStats = helper(half);
-    expect(halfStats.min).toBe(0);
-    expect(halfStats.max).toBe(1);
-    expect(halfStats.mean).toBe(0.5);
-    expect(halfStats.variance).toBe(0.25);
+    expect(halfStats.min).toBeCloseTo(0);
+    expect(halfStats.max).toBeCloseTo(1);
+    expect(halfStats.mean).toBeCloseTo(0.5);
+    expect(halfStats.variance).toBeCloseTo(0.27777);
 
     const numberStats = helper(numbers);
-    expect(numberStats.min).toBe(0);
-    expect(numberStats.max).toBe(9);
-    expect(numberStats.mean).toBe(4.5);
+    expect(numberStats.min).toBeCloseTo(0);
+    expect(numberStats.max).toBeCloseTo(9);
+    expect(numberStats.mean).toBeCloseTo(4.5);
+    expect(numberStats.variance).toBeCloseTo(9.1666);
 });
 
 test("ingestor() calculates Stats of a source of numbers by being fed the values", () => {
@@ -70,27 +82,27 @@ test("ingestor() calculates Stats of a source of numbers by being fed the values
     }
 
     const zeroStats = helper(zeros);
-    expect(zeroStats.min).toBe(0);
-    expect(zeroStats.max).toBe(0);
-    expect(zeroStats.mean).toBe(0);
-    expect(zeroStats.variance).toBe(0);
+    expect(zeroStats.min).toBeCloseTo(0);
+    expect(zeroStats.max).toBeCloseTo(0);
+    expect(zeroStats.mean).toBeCloseTo(0);
+    expect(zeroStats.variance).toBeCloseTo(0);
 
     const oneStats = helper(ones);
-    expect(oneStats.min).toBe(1);
-    expect(oneStats.max).toBe(1);
-    expect(oneStats.mean).toBe(1);
-    expect(oneStats.variance).toBe(0);
+    expect(oneStats.min).toBeCloseTo(1);
+    expect(oneStats.max).toBeCloseTo(1);
+    expect(oneStats.mean).toBeCloseTo(1);
+    expect(oneStats.variance).toBeCloseTo(0);
 
     const halfStats = helper(half);
-    expect(halfStats.min).toBe(0);
-    expect(halfStats.max).toBe(1);
-    expect(halfStats.mean).toBe(0.5);
-    expect(halfStats.variance).toBe(0.25);
+    expect(halfStats.min).toBeCloseTo(0);
+    expect(halfStats.max).toBeCloseTo(1);
+    expect(halfStats.mean).toBeCloseTo(0.5);
+    expect(halfStats.variance).toBeCloseTo(0.27777);
 
     const numberStats = helper(numbers);
-    expect(numberStats.min).toBe(0);
-    expect(numberStats.max).toBe(9);
-    expect(numberStats.mean).toBe(4.5);
+    expect(numberStats.min).toBeCloseTo(0);
+    expect(numberStats.max).toBeCloseTo(9);
+    expect(numberStats.mean).toBeCloseTo(4.5);
 });
 
 test("We can abuse Observe() without a performance hit", () => {
@@ -150,4 +162,180 @@ test("Validate() returns sensible results", () => {
     expect(Validate(oneStats, { tolerance: 0, min: 1, max: 1, mean: 1, variance: 10 })).toBe(false);
     expect(Validate(oneStats, { tolerance: 0.1, min: 1, max: 1, mean: 1, variance: 0.03 })).toBe(true);
     expect(Validate(oneStats, { tolerance: 0.1, min: 1, max: 1, mean: 1, variance: 0.13 })).toBe(false);
+});
+
+test("ObserveCovariance() does joint observation of two sample sets", () => {
+    const zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const ones = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const half = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const supply = (x: number[], y: number[]) => {
+        let idx = 0;
+        return () => {
+            const output = { x: x[idx], y: y[idx] };
+            idx++;
+            return output;
+        };
+    }
+
+    const helper = (x: number[], y: number[]) => ObserveCovariance(supply(x, y), 10);
+
+    const zo = helper(zeros, ones);
+
+    // x is stats for first (zeros).
+    expect(zo.x.min).toBeCloseTo(0);
+    expect(zo.x.max).toBeCloseTo(0);
+    expect(zo.x.mean).toBeCloseTo(0);
+    expect(zo.x.variance).toBeCloseTo(0);
+
+    // y is stats for second (ones).
+    expect(zo.y.min).toBeCloseTo(1);
+    expect(zo.y.max).toBeCloseTo(1);
+    expect(zo.y.mean).toBeCloseTo(1);
+    expect(zo.y.variance).toBeCloseTo(0);
+
+    // Covariance is expected value.
+    expect(zo.covariance).toBeCloseTo(0);
+
+    // Everything still true in reverse.
+    const oz = helper(ones, zeros);
+
+    expect(oz.x.min).toBeCloseTo(1);
+    expect(oz.x.max).toBeCloseTo(1);
+    expect(oz.x.mean).toBeCloseTo(1);
+    expect(oz.x.variance).toBeCloseTo(0);
+
+    expect(oz.y.min).toBeCloseTo(0);
+    expect(oz.y.max).toBeCloseTo(0);
+    expect(oz.y.mean).toBeCloseTo(0);
+    expect(oz.y.variance).toBeCloseTo(0);
+
+    expect(oz.covariance).toBeCloseTo(0);
+
+    const hn = helper(half, numbers);
+
+    expect(hn.x.min).toBeCloseTo(0);
+    expect(hn.x.max).toBeCloseTo(1);
+    expect(hn.x.mean).toBeCloseTo(0.5);
+    expect(hn.x.variance).toBeCloseTo(0.27777);
+
+    expect(hn.y.min).toBeCloseTo(0);
+    expect(hn.y.max).toBeCloseTo(9);
+    expect(hn.y.mean).toBeCloseTo(4.5);
+    expect(hn.y.variance).toBeCloseTo(9.1666);
+
+    expect(hn.covariance).toBeCloseTo(1.3888);
+});
+
+test("GetCovarianceIngestor() returns an {x,y} ingestor that returns CovarianceStats", () => {
+    const zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const ones = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const half = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const zoIngestor = GetCovarianceIngestor();
+    const hnIngestor = GetCovarianceIngestor();
+
+    let zo = NullCovarianceStats();
+    let hn = NullCovarianceStats();
+
+    for (let i = 0; i < 10; i++) {
+        zo = zoIngestor(zeros[i], ones[i]);
+        hn = hnIngestor(half[i], numbers[i]);
+    }
+
+    // x is stats for first (zeros).
+    expect(zo.x.min).toBeCloseTo(0);
+    expect(zo.x.max).toBeCloseTo(0);
+    expect(zo.x.mean).toBeCloseTo(0);
+    expect(zo.x.variance).toBeCloseTo(0);
+
+    // y is stats for second (ones).
+    expect(zo.y.min).toBeCloseTo(1);
+    expect(zo.y.max).toBeCloseTo(1);
+    expect(zo.y.mean).toBeCloseTo(1);
+    expect(zo.y.variance).toBeCloseTo(0);
+
+    // Covariance is expected value.
+    expect(zo.covariance).toBeCloseTo(0);
+
+    expect(hn.x.min).toBeCloseTo(0);
+    expect(hn.x.max).toBeCloseTo(1);
+    expect(hn.x.mean).toBeCloseTo(0.5);
+    // Sample variance = 0.25, N = 10 => Bessel's correction = 0.2777...
+    expect(hn.x.variance).toBeCloseTo(0.27777);
+
+    expect(hn.y.min).toBeCloseTo(0);
+    expect(hn.y.max).toBeCloseTo(9);
+    expect(hn.y.mean).toBeCloseTo(4.5);
+    // Sample variance = 8.25, N = 10 => Bessel's correction = 9.1666...
+    expect(hn.y.variance).toBeCloseTo(9.1666);
+
+    // Sample covariance = 1.25, N = 10 => Bessel's correction = 1.3888...
+    expect(hn.covariance).toBeCloseTo(1.3888);
+});
+
+test("CovarianceValidate(observed, expected) validates a CovarianceStats object (observed) against a CovarianceValidation object (expected", () => {
+    const zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    const ones = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const half = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const zoIngestor = GetCovarianceIngestor();
+    const hnIngestor = GetCovarianceIngestor();
+
+    let zoStats = NullCovarianceStats();
+    let hnStats = NullCovarianceStats();
+
+    for (let i = 0; i < 10; i++) {
+        zoStats = zoIngestor(zeros[i], ones[i]);
+        hnStats = hnIngestor(half[i], numbers[i]);
+    }
+
+    const zVal = { min: 0, max: 0, mean: 0, variance: 0 };
+    const oVal = { min: 1, max: 1, mean: 1, variance: 0 };
+
+    const zoVal1 = {};
+    const zoVal2 = { tolerance: 0.05 };
+    const zoVal3 = { tolerance: 0.05, x: zVal }; // Pass.
+    const zoVal4 = { tolerance: 0.05, x: oVal }; // Fail.
+    const zoVal5 = { tolerance: 0.05, y: oVal }; // Pass.
+    const zoVal6 = { tolerance: 0.05, y: zVal }; // Fail.
+    const zoVal7 = { tolerance: 0.05, x: zVal, y: oVal }; // Pass.
+    const zoVal8 = { tolerance: 0.05, x: oVal, y: zVal }; // Fail.
+    const zoVal9 = { tolerance: 0.05, x: zVal, y: oVal, covariance: 0 }; // Pass.
+
+    expect(CovarianceValidate(zoStats, zoVal1)).toBe(true);
+    expect(CovarianceValidate(zoStats, zoVal2)).toBe(true);
+    expect(CovarianceValidate(zoStats, zoVal3)).toBe(true);
+    expect(CovarianceValidate(zoStats, zoVal4)).toBe(false);
+    expect(CovarianceValidate(zoStats, zoVal5)).toBe(true);
+    expect(CovarianceValidate(zoStats, zoVal6)).toBe(false);
+    expect(CovarianceValidate(zoStats, zoVal7)).toBe(true);
+    expect(CovarianceValidate(zoStats, zoVal8)).toBe(false);
+    expect(CovarianceValidate(zoStats, zoVal9)).toBe(true);
+
+    const hVal = { min: 0, max: 1, mean: 0.5, variance: 0.27777 };
+    const nVal = { min: 0, max: 9, mean: 4.5 };
+
+    const hnVal1 = {};
+    const hnVal2 = { tolerance: 0.05 };
+    const hnVal3 = { tolerance: 0.05, x: hVal }; // Pass.
+    const hnVal4 = { tolerance: 0.05, x: nVal }; // Fail.
+    const hnVal5 = { tolerance: 0.05, y: nVal }; // Pass.
+    const hnVal6 = { tolerance: 0.05, y: hVal }; // Fail.
+    const hnVal7 = { tolerance: 0.05, x: hVal, y: nVal }; // Pass.
+    const hnVal8 = { tolerance: 0.05, x: nVal, y: hVal }; // Fail.
+    const hnVal9 = { tolerance: 0.05, x: hVal, y: nVal, covariance: 1.38888 }; // Pass.
+
+    expect(CovarianceValidate(hnStats, hnVal1)).toBe(true);
+    expect(CovarianceValidate(hnStats, hnVal2)).toBe(true);
+    expect(CovarianceValidate(hnStats, hnVal3)).toBe(true);
+    expect(CovarianceValidate(hnStats, hnVal4)).toBe(false);
+    expect(CovarianceValidate(hnStats, hnVal5)).toBe(true);
+    expect(CovarianceValidate(hnStats, hnVal6)).toBe(false);
+    expect(CovarianceValidate(hnStats, hnVal7)).toBe(true);
+    expect(CovarianceValidate(hnStats, hnVal8)).toBe(false);
+    expect(CovarianceValidate(hnStats, hnVal9)).toBe(true);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+// ---- MIN, MAX, MEAN, VARIANCE ---- //
+
 /** Basic statistical quantities of a sample set (min, max, mean, variance). */
-export interface Stats {
+export type Stats = {
     mean: number;
-    variance: number; // What about Bessel's correction?
+    variance: number;
     min: number;
     max: number;
 }
 
 /** Optional expected values to check in Validate(), plus accepted absolute tolerance. */
-export interface Validation {
-    tolerance: number;
+export type Validation = {
+    tolerance?: number;
     mean?: number;
     variance?: number;
     min?: number;
@@ -47,7 +49,7 @@ export function GetIngestor(): SampleIngestor {
 
         return {
             mean: mean,
-            variance: squareErrors / sampleSize,
+            variance: squareErrors / (sampleSize - 1),
             min: min,
             max: max,
         };
@@ -69,12 +71,128 @@ export function Observe(generator: () => number, sampleSize: number): Stats {
 }
 
 /** Validates the values in 'observed' against the parameters in 'expected'. */
+// TODO: TEST OPTIONAL TOLERANCE.
 export function Validate(observed: Stats, expected: Validation): boolean {
     const helper = (expected: number | undefined, observed: number, tolerance: number): boolean => {
         return expected == undefined || Math.abs(observed - expected) <= tolerance;
     }
-    return helper(expected.mean, observed.mean, expected.tolerance) &&
-        helper(expected.variance, observed.variance, expected.tolerance) &&
-        helper(expected.min, observed.min, expected.tolerance) &&
-        helper(expected.max, observed.max, expected.tolerance);
+    const tolerance = expected.tolerance || Number.EPSILON;
+    return helper(expected.mean, observed.mean, tolerance) &&
+        helper(expected.variance, observed.variance, tolerance) &&
+        helper(expected.min, observed.min, tolerance) &&
+        helper(expected.max, observed.max, tolerance);
+}
+
+// ---- COVARIANCE ---- //
+
+/** Basic statistical quantities of a sample set (min, max, mean, variance). */
+export type CovarianceStats = {
+    x: Stats;
+    y: Stats;
+    covariance: number;
+}
+
+/** Returns a standard CovarianceStats object to be used as a null value. */
+export function NullCovarianceStats(): CovarianceStats {
+    return { x: NullStats(), y: NullStats(), covariance: Number.NaN, };
+}
+
+export type CovarianceIngestor = (x: number, y: number) => CovarianceStats;
+
+/** Progressively calculate individual stats and covariance of two sample sets.
+ *
+ * Returns a function that receives new sample pairs and outputs an updated CovarianceStats
+ * object with the aggregate information of all provided samples.
+ *
+ * Uses a variant of Welford's algorithm to calculate everything in a single pass.
+ */
+export function GetCovarianceIngestor(): CovarianceIngestor {
+    let meanX = 0;
+    let errsX = 0; // Square error sum.
+    let minX = Number.MAX_VALUE;
+    let maxX = -Number.MAX_VALUE;
+
+    let meanY = 0;
+    let errsY = 0; // Square error sum.
+    let minY = Number.MAX_VALUE;
+    let maxY = -Number.MAX_VALUE;
+
+    let sampleSize = 0;
+    let crossErrors = 0;
+
+    return (x: number, y: number) => {
+        sampleSize += 1;
+
+        if (x < minX) minX = x;
+        if (x > maxX) maxX = x;
+        const errPreX = x - meanX;
+        meanX += errPreX / sampleSize;
+        const errPostX = x - meanX;
+        errsX += errPreX * errPostX;
+
+        if (y < minY) minY = y;
+        if (y > maxY) maxY = y;
+        const errPreY = y - meanY;
+        meanY += errPreY / sampleSize;
+        const errPostY = y - meanY;
+        errsY += errPreY * errPostY;
+
+        crossErrors += errPreX * errPostY;
+
+        return {
+            x: {
+                mean: meanX,
+                variance: errsX / (sampleSize - 1),
+                min: minX,
+                max: maxX,
+            },
+            y: {
+                mean: meanY,
+                variance: errsY / (sampleSize - 1),
+                min: minY,
+                max: maxY,
+            },
+            covariance: crossErrors / (sampleSize - 1),
+        };
+    }
+}
+
+/** Calculate individual stats and covariance of two sample sets given by successive calls to generator().
+ *
+ * Uses Welford's algorithm to calculate the means, variances and covariance in a single pass.
+ */
+export function ObserveCovariance(generator: () => { x: number, y: number }, sampleSize: number): CovarianceStats {
+    const ingestor = GetCovarianceIngestor();
+    let stats = NullCovarianceStats();
+    for (let i = 0; i < sampleSize; i++) {
+        const sample = generator();
+        stats = ingestor(sample.x, sample.y);
+    }
+    return stats;
+}
+
+/**
+ * Optional expected values to check in CovarianceValidate(), plus accepted absolute tolerance.
+ *
+ * * The individual Validation objects x and y can have their own tolerance, but it's not recommended.
+ * * The top-level tolerance takes precedence if present.
+ */
+export type CovarianceValidation = {
+    tolerance?: number;
+    x?: Validation;
+    y?: Validation;
+    covariance?: number;
+}
+
+/** Validates the values in 'observed' against the parameters in 'expected'. */
+export function CovarianceValidate(observed: CovarianceStats, expected: CovarianceValidation): boolean {
+    const isGood = (observed: Stats, expected?: Validation, tolerance?: number): boolean =>
+        !expected || Validate(observed, { ...expected, tolerance: tolerance || expected.tolerance });
+
+    const xPass = isGood(observed.x, expected.x, expected.tolerance);
+    const yPass = isGood(observed.y, expected.y, expected.tolerance);
+    const covPass = (expected.covariance == undefined) ||
+            Math.abs(observed.covariance - expected.covariance) <= (expected.tolerance || Number.EPSILON);
+
+    return xPass && yPass && covPass;
 }
